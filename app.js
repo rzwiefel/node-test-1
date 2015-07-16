ab7b3938bf1ee0b8f85622a8bb4e24297d4aefc0
Ryan Zwiefelhofer <ryan.zwiefelhofer@connexta.com>
2015-07-15 20:39:27 -0700

    Added Timestamp to page

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ module.exports = function (port) {
 	var app = express();
 	
 	app.all('*', function (req, res) {
-		res.send('<html><h1>Hello friend</h1></html>');
+		res.send('<html><h1>Hello friend</h1><h2>Welcome to SlackException</h2></br></br><h3>' + new Date().toISOString() + '</h3></html>');
 	});
+	console.log('Starting server on port ' + port);
 	app.listen(port);
+
 }
